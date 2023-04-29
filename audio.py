c20d6d4da94be64b1f373213ffaef51881436025
David <davidhand1988@gmail.com>
2023-04-29 00:01:31 -0400

    Switch up audio scheme

--- audio.py
+++ audio.py
@@ -10,8 +10,8 @@ import time
 import requests
 import wave
 import gtts
-from playsound import playsound
 from pydub import AudioSegment
+from playsound import playsound
 
 def record_audio_for_duration(duration, filename):
     chunk = 1024  # Record in chunks of 1024 samples
@@ -138,8 +138,44 @@ def wave_header(sample_rate, channels, dtype):
 def convert_text_to_audio(text):
     tts = gtts.gTTS(text, lang="en")
     tts.save("response.mp3")
-    playsound("response.mp3")
+    # Load the audio file
+    sound = AudioSegment.from_mp3("response.mp3")
+
+    # Speed up the playback speed by 1.25 times
+    #sound = sound.speedup(playback_speed=1)
+
+    # Play the modified audio file
+    sound.export("response.mp3", format="mp3")
+    play_audio("response.mp3")
+
+def play_audio(filename):
+    # Load the audio file with pydub
+    sound = AudioSegment.from_file(filename, format="mp3")
+
+    # Speed up the playback speed with pydub
+    playback_speed=1.25
+    sound = sound.speedup(playback_speed)
+
+    # Export the modified audio to a wave file
+    sound.export("modified.wav", format="wav")
+
+    # Open the wave file with wave
+    with wave.open("modified.wav", "rb") as audio_file:
+        audio_data = audio_file.readframes(audio_file.getnframes())
+
+    # Play the audio data with pyaudio
+    audio_player = pyaudio.PyAudio()
+    stream = audio_player.open(format=audio_player.get_format_from_width(audio_file.getsampwidth()),
+                               channels=audio_file.getnchannels(),
+                               rate=audio_file.getframerate(),
+                               output=True)
+    stream.write(audio_data)
+
+    # Close the audio stream and the PyAudio object
+    stream.stop_stream()
+    stream.close()
+    audio_player.terminate()
 
 if __name__ == "__main__":
     record_audio_for_duration(5,"test.wav")
-    convert_text_to_audio("Hello, this is a test")
+    convert_text_to_audio("I have extra wood")
